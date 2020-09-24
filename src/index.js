74bcd1d6514508557fd452706f36c4718044ca91
jchen2021 <57500286+jchen2021@users.noreply.github.com>
2020-09-24 12:39:03 -0400

    try

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Hello");
+//get bill input
+const tipForm = document.getElementById("tip-form"); //global scope
+
+tipForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    const billAmount = Number(document.querySelector("#bill-amount").value); //scoped to function, functionscope
+    const tipPercentage = Number(document.querySelector("#tip-amount").value);
+    const finalAmount = Number(document.querySelector("#final-amount").value);
+    const tipAmount = tipPercentage * billAmount;
+    const finalAmount = tipAmount + billAmount;
+    tipAmount.innerHTML = `${finalAmount}`;
+
+    
+
+    //reset the calculator
+    
+    
+});
+
+//null is an intentional absent of the value
+//undefined is you sepll a value wrong
+
+
+//get tip percentage
+
+
+
+//process the values (actual calculations) get the tip
+
+//display 
